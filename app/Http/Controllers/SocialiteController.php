307d15ec80f139b386d0a384ebaf02b6f51b4c1d
RianiDestianti <rianidestiantii@gmail.com>
2025-04-14 22:12:06 +0700

    fix : time

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
 use Laravel\Socialite\Facades\Socialite;
+use Carbon\Carbon;
 
 class SocialiteController extends Controller
 {
@@ -17,33 +17,31 @@ class SocialiteController extends Controller
     }
 
     public function callback()
-{
-    $userFromGoogle = Socialite::driver('google')->stateless()->user();
+    {
+        $userFromGoogle = Socialite::driver('google')->stateless()->user();
 
-    $userFromDb = User::where('google_id', $userFromGoogle->getId())->first();
+        $userFromDb = User::where('google_id', $userFromGoogle->getId())->first();
 
-    if (!$userFromDb) {
-        $userFromDb = new User();
-        $userFromDb->email = $userFromGoogle->getEmail();
-        $userFromDb->google_id = $userFromGoogle->getId();
-        $userFromDb->name = $userFromGoogle->getName();
+        if (!$userFromDb) {
+            $userFromDb = new User();
+            $userFromDb->email = $userFromGoogle->getEmail();
+            $userFromDb->google_id = $userFromGoogle->getId();
+            $userFromDb->name = $userFromGoogle->getName();
+        }
+
+        $userFromDb->last_login_at = Carbon::now();
         $userFromDb->save();
+
+        if ($userFromDb->is_blocked && $userFromDb->blocked_until && Carbon::parse($userFromDb->blocked_until)->isFuture()) {
+            return view('auth.blocked', [
+                'message' => 'Akun kamu diblokir sampai',
+                'blocked_until' => Carbon::parse($userFromDb->blocked_until)->format('d M Y')
+            ]);
+        }
+
+        auth('web')->login($userFromDb);
+        session()->regenerate();
+
+        return redirect('/');
     }
-
-    // cek apakah user diblokir
-    if ($userFromDb->is_blocked && $userFromDb->blocked_until && \Carbon\Carbon::parse($userFromDb->blocked_until)->isFuture()) {
-        return view('auth.blocked', [
-            'message' => 'Akun kamu diblokir sampai',
-            'blocked_until' => \Carbon\Carbon::parse($userFromDb->blocked_until)->format('d M Y')
-        ]);
-    }
-
-    auth('web')->login($userFromDb);
-    session()->regenerate();
-
-    return redirect('/');
-}
-
-
-
 }
